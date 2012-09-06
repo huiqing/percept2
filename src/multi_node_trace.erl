ece5a59d3d37d1fedf3b7683b9fefac93acd53d5
huiqing <hl@kent.ac.uk>
2012-09-06 14:48:42 +0100

    code tidy up.

--- src/multi_node_trace.erl
+++ src/multi_node_trace.erl
@@ -2,32 +2,26 @@
 
 -compile(export_all).
 
+-spec(start([{node(), file:filename()}]) -> ok).
 start(NodeTraceFilePairs) ->
     {Nodes, _Files} = lists:unzip(NodeTraceFilePairs),
     erlang:set_cookie(node(), secret),
     application:start(runtime_tools),
     {ok, Pid} = inviso:start(),
-    io:format("Pid:\n~p\n", [Pid]),
-    {ok, Res1} = inviso:add_nodes(Nodes, mytag),
-    io:format("Res1:\n~p\n", [Res1]),
+    io:format("Starting inviso ok: ~p~n", [Pid]),
+    {ok, Result1} = inviso:add_nodes(Nodes, mytag),
+    io:format("Adding nodes ok: ~p~n", [Result1]),
     InitTraceSetting = [{Node, [{trace, {file, TraceFile}}]}||
                         {Node, TraceFile}<-NodeTraceFilePairs],
-    {ok, Res2} = inviso:init_tracing(InitTraceSetting),
-    io:format("Res2:\n~p\n", [Res2]),
-    {ok, Res3}=inviso:tf(Nodes, all, [send, 'receive']),
-    io:format("Res3:\n~p\n", [Res3]),
-    {ok, Res4}=inviso:tf(all, [timestamp]),
-    io:format("Res4:\n~p\n", [Res4]),
+    {ok, Result2} = inviso:init_tracing(InitTraceSetting),
+    io:format("Initiate tracing ok:~p~n", [Result2]),
+    {ok, Result3}=inviso:tf(Nodes, all, [send, 'receive']),
+    io:format("Setting tracing flags ok:~p~n", [Result3]),
+    {ok, Result4}=inviso:tf(all, [timestamp]),
+    io:format("Setting tracing flags ok:~p~n", [Result4]),
     ok.
 
- 
 stop()->
     inviso:stop_tracing(),
     inviso:stop().
     
-%% fetch_logs(Nodes) ->
-%%     [inviso:fetch_log(Node, ".", "")
-%%      ||Node<-Nodes].
-
-
-%% test_inviso_percept:start([{'server@hl-lt', "server_trace_log.dat"}, {'client@hl-lt', "client_trace_log.dat"}]).
