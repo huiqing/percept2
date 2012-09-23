7f873f27a6f2aa9124f51df64885c35d55f92303
huiqing <hl@kent.ac.uk>
2012-09-23 23:51:29 +0100

    changed to use ttb.

--- src/multi_node_trace.erl
+++ src/multi_node_trace.erl
@@ -2,26 +2,12 @@
 
 -compile(export_all).
 
--spec(start([{node(), file:filename()}]) -> ok).
-start(NodeTraceFilePairs) ->
-    {Nodes, _Files} = lists:unzip(NodeTraceFilePairs),
-    erlang:set_cookie(node(), secret),
-    application:start(runtime_tools),
-    {ok, Pid} = inviso:start(),
-    io:format("Starting inviso ok: ~p~n", [Pid]),
-    {ok, Result1} = inviso:add_nodes(Nodes, mytag),
-    io:format("Adding nodes ok: ~p~n", [Result1]),
-    InitTraceSetting = [{Node, [{trace, {file, TraceFile}}]}||
-                        {Node, TraceFile}<-NodeTraceFilePairs],
-    {ok, Result2} = inviso:init_tracing(InitTraceSetting),
-    io:format("Initiate tracing ok:~p~n", [Result2]),
-    {ok, Result3}=inviso:tf(Nodes, all, [send, 'receive']),
-    io:format("Setting tracing flags ok:~p~n", [Result3]),
-    {ok, Result4}=inviso:tf(all, [timestamp]),
-    io:format("Setting tracing flags ok:~p~n", [Result4]),
+-spec(start([node()]) -> ok).
+start(Nodes) ->
+    Res=ttb:tracer(Nodes),
+    Res1=ttb:p(all, [send, 'receive']), 
     ok.
 
 stop()->
-    inviso:stop_tracing(),
-    inviso:stop().
-    
+    ttb:stop().
+   
