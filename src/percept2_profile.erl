5ce34cd5919bf3e292f3f59445b75c33981d642c
huiqing <hl@kent.ac.uk>
2012-10-19 13:03:40 +0100

    added outputting message.

--- src/percept2_profile.erl
+++ src/percept2_profile.erl
@@ -155,8 +155,10 @@ set_tracer(Port, Opts) ->
     {TraceOpts, ProfileOpts, Mods} = parse_profile_options(Opts),
     MatchSpec = [{'_', [], [{message, {{cp, {caller}}}}]}],
     _Res=[erlang:trace_pattern({Mod, '_', '_'}, MatchSpec, [local])||Mod <- Mods],
-    erlang:trace(all, true, [{tracer, Port}, timestamp, call, return_to, 
+    io:format("Trace patterns set...\n"),  %% do not remove.
+    _Res1=erlang:trace(all, true, [{tracer, Port}, timestamp, call, return_to, 
                              set_on_spawn, procs| TraceOpts]),
+    io:format("Trace flags set...\n"),   %% do not remove.
     erlang:system_profile(Port, ProfileOpts),
     ok.
     
