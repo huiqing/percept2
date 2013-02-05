39f6d66af747477282f2a8a62fb3d7a68260a35a
huiqing <hl@kent.ac.uk>
2013-02-05 14:42:33 +0000

    removed unneeded files; added doc file/.

--- case_studies/multi_nodes/test_node/test_multi_node_trace.erl
+++
@@ -1,9 +0,0 @@
--module(test_multi_node_trace).
-
--compile(export_all).
-
-start() ->
-    multi_node_trace:start(['server@hl-lt', 'client@hl-lt']).
-
-stop() ->
-    multi_node_trace:stop().
--- case_studies/par_sim_code/test/test3.erl
+++
@@ -1,8 +0,0 @@
--module(test3).  
-
-foo(New) ->
-    [self_callout() %%dddd  
-       || not false].
-
-self_callout() ->
-    dd.
