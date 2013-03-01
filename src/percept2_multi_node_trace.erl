7691046705397639001da3d2faadd8246803e4f2
huiqing <hl@kent.ac.uk>
2013-03-01 19:22:16 +0000

    renamed from multi_node_trace.

---
+++ src/percept2_multi_node_trace.erl
@@ -0,0 +1,13 @@
+-module(percept2_multi_node_trace). 
+
+-compile(export_all).
+
+-spec(start([node()]) -> ok).
+start(Nodes) ->
+    _Res=ttb:tracer(Nodes),
+    _Res1=ttb:p(all, [send, 'receive']), 
+    ok.
+
+stop()->
+    ttb:stop().
+   
