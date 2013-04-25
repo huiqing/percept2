ecdd25c93d4a1d5bcaa4ff97243de7f3eec07123
huiqing <hl@kent.ac.uk>
2013-04-25 12:02:23 +0100

    changed back to use the activity record.

--- src/percept2_analyzer.erl
+++ src/percept2_analyzer.erl
@@ -86,49 +86,46 @@ activities2count2(Acts, StartTs) ->
     activities2count2(Acts, StartTs, Start, dict:new(),[]).
 
 activities2count2([], _, _, _, Out) -> lists:reverse(Out);
-activities2count2([{Ts,Id, State} | Acts], StartTs, {Proc,Port}, StateDict, Out) ->
+activities2count2([#activity{id = Id, timestamp = Ts, state = State} | Acts], StartTs, {Proc,Port}, StateDict, Out) ->
     case dict:find(Id, StateDict) of
         {ok, State} ->
-            activities2count2(Acts, StartTs, {Proc, Port}, StateDict, 
+            activities2count2(Acts, StartTs, {Proc, Port}, StateDict,
                               [{?seconds(Ts, StartTs), Proc, Port}|Out]);
         _ ->
-            activities2count3([{Id, Ts,State} | Acts],
+            activities2count3([#activity{id = Id, timestamp = Ts, state = State} | Acts],
                               StartTs, {Proc, Port}, StateDict,Out)
                               %% [{?seconds(Ts, StartTs), Proc, Port}|Out])
     end.
                           
-activities2count3([{Id={pid, _}, Ts, active} | Acts], 
+activities2count3([#activity{id = Id={pid, _}, timestamp = Ts, state = active} | Acts],
                   StartTs, {Proc,Port}, StateDict, Out) ->
     activities2count2(Acts, StartTs, {Proc + 1, Port}, dict:store(Id, active, StateDict),
                       [{?seconds(Ts, StartTs), Proc + 1, Port}|Out]);
-activities2count3([{Id={pid, _}, Ts, inactive} | Acts], 
+activities2count3([#activity{ id =Id={pid, _}, timestamp = Ts, state = inactive} | Acts],
                   StartTs, {Proc,Port}, StateDict, Out) ->
-    activities2count2(Acts, StartTs, {Proc - 1, Port},  dict:store(Id, inactive, StateDict),
+    activities2count2(Acts, StartTs, {Proc - 1, Port}, dict:store(Id, inactive, StateDict),
                       [{?seconds(Ts, StartTs), Proc - 1, Port}|Out]);
-activities2count3([{Id, Ts, active} | Acts],
+activities2count3([#activity{ id = Id, timestamp = Ts, state = active} | Acts],
                   StartTs, {Proc,Port}, StateDict, Out) when is_port(Id) ->
-    activities2count2(Acts, StartTs, {Proc, Port + 1},  dict:store(Id, active, StateDict), 
+    activities2count2(Acts, StartTs, {Proc, Port + 1}, dict:store(Id, active, StateDict),
                       [{?seconds(Ts, StartTs), Proc, Port + 1}|Out]);
-activities2count3([{Id, Ts, nactive} | Acts], 
+activities2count3([#activity{ id = Id, timestamp = Ts, state = inactive} | Acts],
                   StartTs, {Proc,Port}, StateDict, Out) when is_port(Id) ->
-    activities2count2(Acts, StartTs, {Proc, Port - 1},  dict:store(Id, inactive, StateDict),
+    activities2count2(Acts, StartTs, {Proc, Port - 1}, dict:store(Id, inactive, StateDict),
                       [{?seconds(Ts, StartTs), Proc, Port - 1}|Out]).
-
-
-inactive_start_states(Acts) -> 
+inactive_start_states(Acts) ->
     D = activity_start_states(Acts, dict:new()),
     dict:fold(fun
-        ({pid, _}, inactive, {Procs, Ports})  -> {Procs + 1, Ports};
+        ({pid, _}, inactive, {Procs, Ports}) -> {Procs + 1, Ports};
         (K, inactive, {Procs, Ports}) when is_port(K) -> {Procs, Ports + 1};
-        (_, _, {Procs, Ports})                        -> {Procs, Ports}
+        (_, _, {Procs, Ports}) -> {Procs, Ports}
     end, {0,0}, D).
 activity_start_states([], D) -> D;
-activity_start_states([{Id, _Ts,State}|Acts], D) ->
+activity_start_states([#activity{id = Id, state = State}|Acts], D) ->
     case dict:is_key(Id, D) of
-        true  -> activity_start_states(Acts, D);
+        true -> activity_start_states(Acts, D);
         false -> activity_start_states(Acts, dict:store(Id, State, D))
     end.
-
 
 
 %% @spec activities2count(#activity{}, timestamp()) -> Result
@@ -241,7 +238,7 @@ waiting_activities_mfa_list([Activity|Activities], ListedMfas) ->
                             put({waiting_mfa, MFA}, {Total + Waited, [Waited | TimedMfa]}),
 			    ListedMfas
 		    end;
-		[#activity{timestamp=Time2, id = Pid, state = active} | _ ] ->
+		[#activity{timestamp=Time2, id = Pid, state =active} | _ ] ->
 		    % Calculate waiting time
                     Waited = ?seconds(Time2, Time),
 		    % Get previous entry
