f8a799cded0274df20e8f79a8229e5e6ba15c173
huiqing <hl@kent.ac.uk>
2013-12-05 23:36:07 +0000

    removed unused code.

--- src/percept2_orbit.erl
+++ src/percept2_orbit.erl
@@ -14,72 +14,9 @@ analyze_orbit_data(N) ->
     percept2:analyze(Files).
     
 
-%% timestamped event data.
-process_orbit_data()->
-    {{Meg, Sec, Mic},_,_} = ets:first(inter_proc),
-    StartTs = {Meg, Sec, Mic-1},
-    Data = [io_lib:format("~p.\n", [{round(now_diff(Ts, StartTs)/1000), 
-                                    short_node_name(From), 
-                                    short_node_name(To), Size}])
-            ||{_, {Ts, From,_},{To,_},Size}<-ets:tab2list(inter_proc),
-              From /= nonode, To/=nonode],
-    Str = lists:flatten(Data),
-    file:write_file("inter_node.txt", list_to_binary(Str)).          
+%% percept2_orbit:percept2_orbit:run_orbit_with_trace(15).
+%% percept2_orbit:analyze_orbit_data(15).
 
-process_orbit_data_sum()->
-    {{Meg, Sec, Mic},_,_} = ets:first(inter_proc),
-    StartTs = {Meg, Sec, Mic-1},
-    Data = [{round(now_diff(Ts, StartTs)/1000), 
-             short_node_name(From), 
-             short_node_name(To), Size}||
-               {_, {Ts, From,_},{To,_},Size}
-                   <-ets:tab2list(inter_proc), 
-               From/=nonode,
-               To/=nonode],
-    Data1=process_orbit_data_sum_1(Data, 0, [], []),
-    Str=lists:flatten([io_lib:format("~p.\n", [E])||E<-Data1]),
-    file:write_file("inter_node_sum.txt", list_to_binary(Str)).
-    
-    
-%% summary data every 200 millisecods.    
-process_orbit_data_sum_1([],StartTime, Cur, Acc) ->
-    lists:reverse([{StartTime+200, Cur}|Acc]);
-process_orbit_data_sum_1([{Time, From, To, MsgSize}|Data], 
-                         StartTime, Cur, Acc) ->
-    NextStartTime = StartTime +200,
-    {Node1, Node2} =if From < To -> {From, To};
-                       true -> {To, From}
-                    end,
-    if Time=< NextStartTime ->
-            NewCur=case lists:keyfind({Node1, Node2}, 1, Cur) of 
-                       {{Node1, Node2}, Cnt, SumSize} -> 
-                           lists:keyreplace({Node1, Node2}, 1, Cur, 
-                                            {{Node1, Node2}, Cnt+1, 
-                                             MsgSize+SumSize});
-                       false ->
-                           [{{Node1,Node2}, 1, MsgSize}|Cur]
-                   end,
-            process_orbit_data_sum_1(Data, StartTime, NewCur, Acc);
-       true ->
-            process_orbit_data_sum_1(Data, NextStartTime, 
-                                     [{{Node1, Node2}, 1, MsgSize}], 
-                                     [{NextStartTime,Cur}|Acc])
-    end.
-
-  
-now_diff(EndTS,StartTS) ->
-    try timer:now_diff(EndTS, StartTS)
-    catch _E1:_E2 -> 0
-    end.
-   
-
-short_node_name(NodeName) ->
-    Str = atom_to_list(NodeName),
-    Index = string:chr(Str, $@),
-    if Index ==0 ->
-            NodeName;
-       true -> list_to_atom(string:substr(Str,1, Index-1))
-    end.
 
 
 %% How to use: 
